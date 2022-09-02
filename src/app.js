be2652e11a17c4e0ead367b78278a1c7260da176
frako23 <frako23@gmail.com>
2022-09-02 16:05:41 +0000

    se agrega el proyecto

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let quien = ["perro", "hijo", "hermanito", "vecino"];
+  let accion = ["se comió", "rompió", "explotó", "perdió"];
+  let que = ["mi tarea", "tu celular", "tu carro", "tu laptop"];
+
+  let excuseP = document.querySelector("#excuse");
+  excuseP.textContent =
+    "Mi" +
+    " " +
+    quien[Math.floor(Math.random() * 4)] +
+    " " +
+    accion[Math.floor(Math.random() * 4)] +
+    " " +
+    que[Math.floor(Math.random() * 4)];
+
   console.log("Hello Rigo from the console!");
 };
